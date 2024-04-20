a6acd278249cd2a7b3fbfd89db260ad61c185b4e
Rebekah <macrosystem@proton.me>
2024-04-20 22:13:13 +0200

    Create d10-binary.py

---
+++ d10-binary.py
@@ -0,0 +1,12 @@
+my_file = open("d10", r)
+my_file = str(my_file)
+
+my_binary_output = ""
+  
+for my_char in my_file:
+    if(int(my_char) % 2 = 0):
+        my_binary_output.append("0")
+    else:
+        my_binary_outpud.append("1")
+
+print(my_binary_output)
